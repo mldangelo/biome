fe1c3780f16d4abf6a9891c2d92852c33d59ba6b
Carson McManus <carson.mcmanus1@gmail.com>
2025-12-27 10:58:46 -0500

    feat(analyze/html/vue): add `useVueVForKey`

--- packages/@biomejs/backend-jsonrpc/src/workspace.ts
+++ packages/@biomejs/backend-jsonrpc/src/workspace.ts
@@ -2192,6 +2192,11 @@ See <https://biomejs.dev/linter/rules/use-vue-multi-word-component-names>
 	 */
 	useVueMultiWordComponentNames?: UseVueMultiWordComponentNamesConfiguration;
 	/**
+	* Enforce that elements using v-for also specify a unique key.
+See <https://biomejs.dev/linter/rules/use-vue-v-for-key> 
+	 */
+	useVueVForKey?: UseVueVForKeyConfiguration;
+	/**
 	* Enforce valid Vue \<template> root usage.
 See <https://biomejs.dev/linter/rules/use-vue-valid-template-root> 
 	 */
@@ -3868,6 +3873,9 @@ export type UseVueHyphenatedAttributesConfiguration =
 export type UseVueMultiWordComponentNamesConfiguration =
 	| RulePlainConfiguration
 	| RuleWithUseVueMultiWordComponentNamesOptions;
+export type UseVueVForKeyConfiguration =
+	| RulePlainConfiguration
+	| RuleWithUseVueVForKeyOptions;
 export type UseVueValidTemplateRootConfiguration =
 	| RulePlainConfiguration
 	| RuleWithUseVueValidTemplateRootOptions;
@@ -5408,6 +5416,10 @@ export interface RuleWithUseVueMultiWordComponentNamesOptions {
 	level: RulePlainConfiguration;
 	options?: UseVueMultiWordComponentNamesOptions;
 }
+export interface RuleWithUseVueVForKeyOptions {
+	level: RulePlainConfiguration;
+	options?: UseVueVForKeyOptions;
+}
 export interface RuleWithUseVueValidTemplateRootOptions {
 	fix?: FixKind;
 	level: RulePlainConfiguration;
@@ -6740,6 +6752,7 @@ export interface UseVueMultiWordComponentNamesOptions {
 	 */
 	ignores?: string[];
 }
+export type UseVueVForKeyOptions = {};
 export type UseVueValidTemplateRootOptions = {};
 export type UseVueValidVBindOptions = {};
 export type UseVueValidVCloakOptions = {};
@@ -7549,6 +7562,7 @@ export type Category =
 	| "lint/nursery/useVueDefineMacrosOrder"
 	| "lint/nursery/useVueHyphenatedAttributes"
 	| "lint/nursery/useVueMultiWordComponentNames"
+	| "lint/nursery/useVueVForKey"
 	| "lint/nursery/useVueValidTemplateRoot"
 	| "lint/nursery/useVueValidVBind"
 	| "lint/nursery/useVueValidVCloak"
